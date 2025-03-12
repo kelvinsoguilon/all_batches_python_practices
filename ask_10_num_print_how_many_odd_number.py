20c917cb22361468837b776fbcd02d34ac6bc76d
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-12 18:40:24 +0800

    Added proccess to identify odd number, Print the output

--- ask_10_num_print_how_many_odd_number.py
+++ ask_10_num_print_how_many_odd_number.py
@@ -3,4 +3,8 @@ odd_number = 0
 #Ask for 10 numbers
 for i in range(10):
     num = int(input("Enter a number: "))
+
+    if num % 2 == 1: #process to identify if a number is odd
+        odd_number += 1 
 #Print the number of odd numbers
+print("The number of odd numbers entered is: ", odd_number)
