719858307f68a7df88ecda354cf1e83303379057
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-12 23:12:56 +0800

    Identify whether number is equal, Print output

--- Batch_2/ask_two_number_print_not_equal.py
+++ Batch_2/ask_two_number_print_not_equal.py
@@ -2,4 +2,5 @@
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
 #identify whether number input were not equal
-#print output
+if num1 != num2:
+    print("Not Equal.") #print output
