3c375c6a7f210a3fdaaca49a09ae02b65035f1dd
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-12 18:19:17 +0800

    No errors, Print quotient

--- print_quotient_two_numbers.py
+++ print_quotient_two_numbers.py
@@ -1,5 +1,5 @@
 #Ask for 2 numbers
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter a number: "))
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter a number: "))
 #Print the quotient
 print("The quotient of the 2 numbers is: ", (num1 / num2))
