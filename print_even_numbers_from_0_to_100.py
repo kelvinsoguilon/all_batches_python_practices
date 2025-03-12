10fe8ee51b2b1608b754e92df091c01a2fc43e85
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-12 18:50:30 +0800

    Added the process, Changed variable name to easily identify

--- print_even_numbers_from_0_to_100.py
+++ print_even_numbers_from_0_to_100.py
@@ -1,5 +1,7 @@
 #store the even numbers
+even_number = []
 #use for loop to iterate 100 numbers
-for i in range(100):
-    #check if num is even
+for numbers in range(100):
+    if i % 2 == 0: #check if num is even
+        even_number.append(numbers)
 #print the even numbers
