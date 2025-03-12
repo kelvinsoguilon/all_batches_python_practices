67e3c0b46b889060422bc8f4bb76dcc0df3c27b6
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-12 23:31:51 +0800

    Print output

--- Batch_2/print_remainder_when_first_num_divide_second_num.py
+++ Batch_2/print_remainder_when_first_num_divide_second_num.py
@@ -2,3 +2,4 @@
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
 #process and print output
+print("The remainder when first number is divided to second number is: ", (num1 % num2))
