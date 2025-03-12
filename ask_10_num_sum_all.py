015cccc2376b541dcaf187019a81984057e22ef3
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-12 18:32:31 +0800

    Added the summation process, Print the result

--- ask_10_num_sum_all.py
+++ ask_10_num_sum_all.py
@@ -3,4 +3,6 @@ sum = 0
 #Ask for 10 numbers
 for i in range(10):
     num = int(input("Enter a number: "))
+    sum += num #add the number to the previous input
 #Print sum of all
+print("The sum of the 10 numbers is: ", sum)
