51e1b61f75979e64e21d3624bb7ea90ee11c0628
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 11:52:24 +0800

    Store odd numbers, Append odd numbers to list

--- Batch_2/print_odd_numbers_from_0_to_100.py
+++ Batch_2/print_odd_numbers_from_0_to_100.py
@@ -1,3 +1,9 @@
 #store the odd numbers
+odd_numbers = []
 #use while loop to print numbers to 100
+count = 0
+while count < 100:
+    count += 1
+    if count % 2 == 1:
+        odd_numbers.append(count)
 #print output
