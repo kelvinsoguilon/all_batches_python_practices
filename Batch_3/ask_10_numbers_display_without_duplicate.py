385b131544f4e98135fae797f8e3f4cc911d37b8
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 13:32:54 +0800

    Check whether number count is 1, Print output

--- Batch_3/ask_10_numbers_display_without_duplicate.py
+++ Batch_3/ask_10_numbers_display_without_duplicate.py
@@ -5,4 +5,6 @@ for i in range(10):
     num = int(input("Enter a number: "))
     numbers.append(num)
 #identify whether a number appeared only once
+unique_number = [num for num in numbers if numbers.count(num) == 1]
 #print output
+print("The numbers without duplicate are: ", unique_number)
