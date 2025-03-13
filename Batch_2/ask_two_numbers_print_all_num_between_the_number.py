c0456265abcd9e4b9130ddfa501b719894c96d76
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 12:13:52 +0800

    Sorted input in ascending order, Print result

--- Batch_2/ask_two_numbers_print_all_num_between_the_number.py
+++ Batch_2/ask_two_numbers_print_all_num_between_the_number.py
@@ -2,3 +2,7 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 #print all the numbers between the input
+num1, num2 = sorted([num1, num2]) #arrange the input in ascending order
+print(f"The numbers between {num1} and {num2} are: ")
+for numbers in range(num1 + 1, num2):
+    print(numbers, end = "\n")
