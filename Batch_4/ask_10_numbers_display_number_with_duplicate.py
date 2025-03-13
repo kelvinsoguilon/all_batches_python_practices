c7198b3b8645b5287a46184462792f839e54fd05
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 15:23:41 +0800

    Added variable to store inputs, and ask for 10 numbers

--- Batch_4/ask_10_numbers_display_number_with_duplicate.py
+++ Batch_4/ask_10_numbers_display_number_with_duplicate.py
@@ -1,4 +1,8 @@
 #store unique and duplicates
+unique_num = []
+duplicate_num = set()
 #ask 10 numbers
+for i in range(10):
+    num = int(input("Enter a number: "))
 #check whether number is unique
 #print output
