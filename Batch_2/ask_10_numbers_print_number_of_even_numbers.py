9744572ba4ba26738e5309bb59e8fc356cb2c58a
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 11:42:53 +0800

    Identify if number is even. Print Result

--- Batch_2/ask_10_numbers_print_number_of_even_numbers.py
+++ Batch_2/ask_10_numbers_print_number_of_even_numbers.py
@@ -4,4 +4,7 @@ even_numbers = 0
 for num in range(10):
     user = int(input("Enter a number: "))
 #identify if number is even
+    if user % 2 == 0:
+        even_numbers += 1
 #print output
+print("The number of even numbers is: ", even_numbers)
