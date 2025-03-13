fadb3c68f6bbea34a211b37fe72c965799893de7
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 11:30:49 +0800

    Completed first and second step

--- Batch_2/subtract_first_num_to_remaining_numbers.py
+++ Batch_2/subtract_first_num_to_remaining_numbers.py
@@ -1,5 +1,8 @@
 #variable to store inputs
+numbers = []
 #ask 10 numbers
+for num in range(10):
+    user = int(input("Enter a number: "))
 #set variable to first input
 #iterate first input to subtract to remaining numbers
 #print result
