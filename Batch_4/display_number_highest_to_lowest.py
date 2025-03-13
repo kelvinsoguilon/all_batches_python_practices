81e8bb37a5fbb6a3a4f5a9d0d49ffe20b69dfff0
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 16:24:41 +0800

    Added variable to store inputs, continuously ask until invalid input

--- Batch_4/display_number_highest_to_lowest.py
+++ Batch_4/display_number_highest_to_lowest.py
@@ -1,4 +1,12 @@
 #store inputs
+numbers = []
 #ask numbers until invalid
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    #handle invalid input
+    except ValueError:
+        break
 #sort numbers from highest to lowest
 #print output
