b044aab1eceb1483dde4026890c53056ba842d83
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 16:29:21 +0800

    Sort numbers from high to low, Print output

--- Batch_4/display_number_highest_to_lowest.py
+++ Batch_4/display_number_highest_to_lowest.py
@@ -9,4 +9,6 @@ while True:
     except ValueError:
         break
 #sort numbers from highest to lowest
+numbers.sort(reverse= True)
 #print output
+print("The numbers entered from highest to lowest are: ", numbers)
