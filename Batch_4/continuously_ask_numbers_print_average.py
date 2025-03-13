5ec0c6de618d9013e70dd549a61ff6c891b08568
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 16:38:42 +0800

    Added variable that stores input, and continuously ask user a number

--- Batch_4/continuously_ask_numbers_print_average.py
+++ Batch_4/continuously_ask_numbers_print_average.py
@@ -1,4 +1,12 @@
 #store inputs
+numbers = []
 #continuously ask until invalid
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    #handle invalid input
+    except ValueError:
+        break
 #average the numbers
 #print result
