d0b35b465196b97ac73d12ec0f9a9159777d9568
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 14:24:08 +0800

    Added store input, while loop, invalid condition, and handle value error

--- Batch_3/display_number_lowest_to_highest.py
+++ Batch_3/display_number_lowest_to_highest.py
@@ -1,5 +1,16 @@
 #store inputs
+numbers = []
 #ask continuously until invalid
-#invalid condition
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        #invalid condition
+        if num < 0:
+            break
+        #move input to list
+        numbers.append(num)
+    #handle ValueError
+    except ValueError:
+        print("Invalid input.")
 #sort input from low to high
 #print output
