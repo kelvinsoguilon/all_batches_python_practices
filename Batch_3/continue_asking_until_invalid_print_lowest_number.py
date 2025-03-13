cf8c8eefdb2c26849aec308845887f46aa401604
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 14:14:05 +0800

    Added variable to store, invalid condition, and handle ValueError

--- Batch_3/continue_asking_until_invalid_print_lowest_number.py
+++ Batch_3/continue_asking_until_invalid_print_lowest_number.py
@@ -1,4 +1,14 @@
 #store inputs
+numbers = []
 #while loop to ask continuously
-#invalid condition
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+        #invalid condition
+        if num < 0:
+            break
+    #Handle error
+    except ValueError:
+        print("Invalid input.")
 #print lowest number
