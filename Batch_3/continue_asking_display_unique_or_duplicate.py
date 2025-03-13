9844f94e6632b97b4794360a0ed458f123537ec2
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 13:53:19 +0800

    Added variable to store inputs, Continue asking until invalid.

--- Batch_3/continue_asking_display_unique_or_duplicate.py
+++ Batch_3/continue_asking_display_unique_or_duplicate.py
@@ -1,5 +1,12 @@
 #store input and duplicates
+unique_num = []
+duplicate_num = set()
 #ask number until invalid
-#invalid condition
-#check whether first entry and print unique
-#check whether duplicate and print duplicate
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        #invalid condition
+        if num < 0:
+            break
+        #check whether first entry and print unique
+        #check whether duplicate and print duplicate
