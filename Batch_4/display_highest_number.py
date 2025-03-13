725f60748dbcf47bb81466c77e2d7ef5d6531d72
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 16:18:38 +0800

    Added variable to store inputs, ask continuously until invalid

--- Batch_4/display_highest_number.py
+++ Batch_4/display_highest_number.py
@@ -1,3 +1,11 @@
 #store inputs
+numbers = []
 #ask continuously until invalid
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    #handle invalid input
+    except ValueError:
+        break    
 #print output
