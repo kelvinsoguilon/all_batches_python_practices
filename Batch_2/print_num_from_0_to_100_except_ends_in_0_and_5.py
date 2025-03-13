459fc0d7959fd83c1a959168adc85bfbc6691839
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 12:05:21 +0800

    Iterate numbers from 0 to 100, identify if ends in 0 and 5. Print Result.

--- Batch_2/print_num_from_0_to_100_except_ends_in_0_and_5.py
+++ Batch_2/print_num_from_0_to_100_except_ends_in_0_and_5.py
@@ -1,2 +1,5 @@
 #use for loop to iterate 100 numbers
-#print result
+print("The numbers from 0 to 100 except ending in 0 and 5 are: ")
+for numbers in range(100):
+    if numbers % 5 != 0:
+        print(numbers, end=",")    #print result
