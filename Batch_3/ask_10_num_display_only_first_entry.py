b959a7cbb323aca9c4fb204889e22aff5b70b07f
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 13:46:50 +0800

    Check number occurence, Print output

--- Batch_3/ask_10_num_display_only_first_entry.py
+++ Batch_3/ask_10_num_display_only_first_entry.py
@@ -4,4 +4,9 @@ duplicated_number = set()
 #ask for 10 numbers
 for i in range(10):
     num = int(input("Enter a number: "))
+    #check if number already entered
+    if num not in duplicated_number:
+        numbers.append(num)
+        duplicated_number.add(num)
 #print result
+print("The numbers displayed only in their first occurence are: ", numbers)
