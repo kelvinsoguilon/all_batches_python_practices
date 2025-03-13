2f43b52e515058c3541bc4a3e0d00e209a29ed90
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 15:26:13 +0800

    Checks whether number already entered. Print output

--- Batch_4/ask_10_numbers_display_number_with_duplicate.py
+++ Batch_4/ask_10_numbers_display_number_with_duplicate.py
@@ -4,5 +4,9 @@ duplicate_num = set()
 #ask 10 numbers
 for i in range(10):
     num = int(input("Enter a number: "))
-#check whether number is unique
+    #check whether number is unique
+    if num in unique_num:
+        duplicate_num.add(num)
+    unique_num.append(num)
 #print output
+print("The numbers with duplicates are: ", duplicate_num)
