3dffd936c64ac7c2d7a6ddcb3e83773a1651b38a
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 13:30:22 +0800

    Added variable to store input, Ask 10 numbers and append to the list

--- Batch_3/ask_10_numbers_display_without_duplicate.py
+++ Batch_3/ask_10_numbers_display_without_duplicate.py
@@ -1,4 +1,8 @@
 #store input
+numbers = []
 #ask for 10 numbers
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
 #identify whether a number appeared only once
 #print output
