024717347680a099e27f6e62b7d9573a53794d08
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-13 15:51:16 +0800

    Store inputs, ask for numbers continuously until invalid

--- Batch_4/display_number_with_most_duplicate.py
+++ Batch_4/display_number_with_most_duplicate.py
@@ -1,5 +1,13 @@
 #store input
+numbers = []
 #ask continuously
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    #invalid condition
+    except ValueError:
+        break
 #check the frequency of numbers
 #find the number with the highest frequency
 #print output
